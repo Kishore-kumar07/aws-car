096dbd5f204c141539c37b5c86447ce89cff133e
Ubuntu <ubuntu@ip-172-31-1-127.ap-southeast-2.compute.internal>
2024-03-24 16:02:23 +0000

    till camera

--- carproject/urls.py
+++ carproject/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from .views import welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',welcome,name="welcome")
+    path('',welcome,name="welcome"),
+    path('authorization',include('authorization.urls'))
 ]
--- carproject/views.py
+++ carproject/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 
 def welcome(request):
-    return render(request, 'authorization/auth.html')
+    return render(request, 'authorization/welcome.html')
+
+def my_redirect_view(request):
+    # Define the URL you want to redirect to
+    redirect_url = "/authorization"
+    return redirect(redirect_url)
